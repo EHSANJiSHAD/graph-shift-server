8fd3904d1c203278fc4523579a0bfe661c195ed2
EHSANJiSHAD <tanvirehsan60@gmail.com>
2022-05-29 22:08:57 +0600

    admin role added

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vbucp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      return res.status(401).send({ message: 'Unauthorized Access' })
+  }
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+          return res.status(403).send({ message: 'Forbidden Access' })
+      }
+      req.decoded = decoded;
+      next();
+  });
+}
 async function run() {
 
   try {
@@ -76,7 +91,8 @@ async function run() {
     app.get('/order', async (req, res) => {
       const buyer = req.query.buyer;
       // console.log(buyer)
-
+      // const authorization = req.headers.authorization;
+      // console.log(authorization);
       const query = { buyer: buyer };
       // console.log(query)
       const result = await orderCollection.find(query).toArray();
@@ -129,6 +145,28 @@ async function run() {
       const result = await newUserCollection.updateOne(filter, updatedDoc, options);
       const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
       res.send({ result, token });
+  })
+
+    app.get('/newUser',async(req,res)=>{
+      const users = await newUserCollection.find().toArray()
+      res.send(users)
+    })
+
+
+
+    app.put('/newUser/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      // const requester = req.decoded.email;
+      // requesterAccount = await newUserCollection.findOne({ email: requester })
+       
+          const filter = { email: email };
+          const updatedDoc = {
+              $set: { role: 'admin' },
+          }
+          const result = await newUserCollection.updateOne(filter, updatedDoc);
+          res.send(result);
+      
+
   })
     ///////////////TOKEN AND ADMIN/////////////////////
 
